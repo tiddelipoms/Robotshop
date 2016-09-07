ceddaa5b57cb939752d9dd0dc04b1d8264f33e1a
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-07 11:18:13 +0200

    skrivet med variabler

--- shop.py
+++ shop.py
@@ -1,1 +1,10 @@
-print(900 * 2 * 1.25 + 100 * 10.6)
+robot_price = 900
+robot_count = 2
+robot_tax = 1.25
+
+book_price = 100
+book_count = 1
+book_tax =1.06
+
+print( 
+	robot_price * robot_count * robot_tax + book_price * book_count * book_tax)
