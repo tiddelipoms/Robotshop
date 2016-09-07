5eeb0a77c20a7b22aa5f9241a304048e0c56ea07
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-07 13:51:01 +0200

    Med klass och metod

--- shop.py
+++ shop.py
@@ -1,8 +1,11 @@
 
-class Product:
+class Product():
 	price = 0
 	count = 0
 	tax = 1
+	
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
 
 robot = Product()
 robot.price = 900
@@ -15,6 +18,5 @@ book.count = 1
 book.tax = 1.06
 
 print( 
-	robot.price * robot.count * robot.tax + 
-	book.price * book.count * book.tax
+	robot.price_with_tax() + book.price_with_tax()
 	)
