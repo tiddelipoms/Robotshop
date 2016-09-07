82a15f4c534f14ea0c3f9b8e212795c154ee02ff
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-07 11:44:29 +0200

    Lagt in citationstecken
    Viktigt att ha i print

--- shop.py
+++ shop.py
@@ -3,6 +3,6 @@ robot = {"price":900, "count":2, "tax":1.25}
 book = {"price":100, "count":1, "tax":1.06}
 
 print( 
-	robot[price] * robot[count] * robot[tax] + 
-	book[price] * book[count] * book[tax]
+	robot["price"] * robot["count"] * robot["tax"] + 
+	book["price"] * book["count"] * book["tax"]
 	)
