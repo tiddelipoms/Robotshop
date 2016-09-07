e6e46ac698290efedbcf91b44655abd789f165f0
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-07 11:41:19 +0200

    Dictionary for products

--- shop.py
+++ shop.py
@@ -1,10 +1,8 @@
-robot_price = 900
-robot_count = 2
-robot_tax = 1.25
 
-book_price = 100
-book_count = 1
-book_tax =1.06
+robot = {"price":900, "count":2, "tax":1.25}
+book = {"price":100, "count":1, "tax":1.06}
 
 print( 
-	robot_price * robot_count * robot_tax + book_price * book_count * book_tax)
+	robot[price] * robot[count] * robot[tax] + 
+	book[price] * book[count] * book[tax]
+	)
